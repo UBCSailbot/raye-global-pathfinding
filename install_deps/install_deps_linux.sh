25c643b1c168746cf9fa3999e43725d3c1db73ae
Denis Trailin <denis.trailin@gmail.com>
2018-09-21 23:18:09 -0700

    Update install_deps_linux.sh

--- install_deps/install_deps_linux.sh
+++ install_deps/install_deps_linux.sh
@@ -31,20 +31,9 @@ if [  $(apt-cache show libprotobuf-dev | grep -Po '(?<=Version: )[0-9]') -gt 2 ]
 else
      # Make sure you grab the latest version
     curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip
-
     # Unzip
-    unzip protoc-3.6.1-linux-x86_64.zip -d protoc3
-
-    # Move protoc to /usr/local/bin/
-    sudo mv protoc3/bin/* /usr/local/bin/
-
-    # Move protoc3/include to /usr/local/include/
-    sudo mv protoc3/include/* /usr/local/include/
-    sudo ln -s /usr/local/bin/protoc /usr/bin/protoc
-    
-    sudo chown $USER /usr/local/bin/protoc
-    sudo chown -R $USER /usr/local/include/google
-    sudo chmod -R 444 /usr/local/include/google/
+    unzip -o protoc-3.6.1-linux-x86_64.zip -d /usr/local bin/protoc
+    rm -f protoc-3.6.1-linux-x86_64.zip
 fi
 
 
