346d2f92235e0ef6542347a2e4fe95d10e7a0e6d
Denis Trailin <denis.trailin@gmail.com>
2018-09-21 21:58:22 -0700

    Update install_deps_linux.sh

--- install_deps/install_deps_linux.sh
+++ install_deps/install_deps_linux.sh
@@ -30,20 +30,16 @@ if [  $(apt-cache show libprotobuf-dev | grep -Po '(?<=Version: )[0-9]') -gt 2 ]
     sudo apt install libprotobuf-dev protobuf-compiler -y
 else
      # Make sure you grab the latest version
-    curl -OL https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip
+    curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_32.zip
 
     # Unzip
-    unzip protoc-3.3.0-linux-x86_64.zip -d protoc3
+    unzip protoc-3.6.1-linux-x86_32.zip -d protoc3
 
     # Move protoc to /usr/local/bin/
     sudo mv protoc3/bin/* /usr/local/bin/
 
     # Move protoc3/include to /usr/local/include/
     sudo mv protoc3/include/* /usr/local/include/
-
-    # Optional: change owner
-    sudo chown $USER /usr/local/bin/protoc
-    sudo chown -R $USER /usr/local/include/google
 fi
 
 # CMake is needed to actually build the system
