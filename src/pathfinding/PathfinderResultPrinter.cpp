16b95cd1293b07525ac1d3c503d315a4236c3133
Håkon Vågsether <haakonsv@gmail.com>
2019-10-16 22:44:49 -0700

    Remove space in KML output

--- src/pathfinding/PathfinderResultPrinter.cpp
+++ src/pathfinding/PathfinderResultPrinter.cpp
@@ -48,8 +48,8 @@ std::string PathfinderResultPrinter::PrintKML(HexPlanet &planet, const Pathfinde
 
   for (HexVertexId id : result.path) {
     const auto &coord = planet.vertex(id).coordinate;
-    handle << coord.to_string_longitude() << ", " << coord.to_string_latitude() << std::endl;
-    ss << coord.to_string_longitude() << ", " << coord.to_string_latitude() << std::endl;
+    handle << coord.to_string_longitude() << "," << coord.to_string_latitude() << std::endl;
+    ss << coord.to_string_longitude() << "," << coord.to_string_latitude() << std::endl;
   }
 
   handle << "</coordinates></LineString></Placemark></Document></kml>\n";
