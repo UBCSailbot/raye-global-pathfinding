94499078c088082af9f5a52ffd517730b5a87d3a
Kieran <kpfgallagher@gmail.com>
2020-03-14 14:48:27 -0700

    Added listener, talker, changed CMakeLists

--- lib/core_libs.cmake
+++ lib/core_libs.cmake
@@ -7,9 +7,9 @@ set(CORE_LIBS)
 list(APPEND CORE_LIBS ${CMAKE_THREAD_LIBS_INIT})
 
 # Generates source for shared message data types using protobuf
-add_subdirectory(${CMAKE_SOURCE_DIR}/lib/protofiles)
-include_directories(${ProtobufIncludePath})
-list(APPEND CORE_LIBS protofiles)
+#add_subdirectory(${CMAKE_SOURCE_DIR}/lib/protofiles)
+#include_directories(${ProtobufIncludePath})
+#list(APPEND CORE_LIBS protofiles)
 
 find_package(Boost 1.58 REQUIRED)
 include_directories(${Boost_INCLUDE_DIR})
@@ -31,13 +31,13 @@ list(APPEND CORE_LIBS ${CURL_LIBRARIES})
 
 
 # Disable building extras we won't need (pure C++ project)
-set(NANOGUI_BUILD_EXAMPLE OFF CACHE BOOL " " FORCE)
-set(NANOGUI_BUILD_PYTHON  OFF CACHE BOOL " " FORCE)
-set(NANOGUI_INSTALL       OFF CACHE BOOL " " FORCE)
+#set(NANOGUI_BUILD_EXAMPLE OFF CACHE BOOL " " FORCE)
+#set(NANOGUI_BUILD_PYTHON  OFF CACHE BOOL " " FORCE)
+#set(NANOGUI_INSTALL       OFF CACHE BOOL " " FORCE)
 
 # Add the configurations from nanogui
-add_subdirectory(lib/nanogui)
-include_directories(lib/nanogui/include)
+#add_subdirectory(lib/nanogui)
+#include_directories(lib/nanogui/include)
 
 # For reliability of parallel build, make the NanoGUI targets dependencies
-set_property(TARGET glfw glfw_objects nanogui PROPERTY FOLDER "dependencies")
+#set_property(TARGET glfw glfw_objects nanogui PROPERTY FOLDER "dependencies")
