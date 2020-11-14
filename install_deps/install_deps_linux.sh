f7d184a0bac49f2fd4e6924cbf13d7e054bc7635
Tyler Lum <tylergwlum@gmail.com>
2020-11-14 01:28:05 -0800

    Test changing azure pipelines 6

--- install_deps/install_deps_linux.sh
+++ install_deps/install_deps_linux.sh
@@ -17,30 +17,20 @@ set -e
 #   has your private key loaded. For reference, see e.g.
 #   https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
 #
-echo "STARTING THE SCRIPT"
-pwd
-ls
-echo "=========="
-ls lib/nanogui
 
 ubuntu_version=`lsb_release -rs | sed 's/\.//'`
-echo "STARTING THE SCRIPT"
 
 # Clone submodules
 git submodule update --init --recursive
-echo "STARTING THE SCRIPT"
 
 # Update package manager indices to latest available.
 sudo apt-get update
-echo "STARTING THE SCRIPT"
 
 # Get the required packages for build
 sudo apt-get install curl libcurl4-gnutls-dev build-essential clang libboost-dev libboost-program-options-dev libglew-dev libglm-dev libeigen3-dev cppcheck xorg-dev libglu1-mesa-dev cmake -y
 
-echo "STARTING THE SCRIPT"
 if [[ $ubuntu_version > 1610 ]]; then
     sudo apt-get -y install libeccodes-dev libeccodes-tools libopenjp2-7-dev
-echo "STARTING THE SCRIPT"
 else
     wget "https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.10.0-Source.tar.gz"
     tar -xzf eccodes-2.10.0-Source.tar.gz
@@ -60,4 +50,3 @@ INSTALL_DEPS_DIRECTORY=${BASH_SOURCE%/*}
 LIB_DIRECTORY=${INSTALL_DEPS_DIRECTORY}/../lib
 
 ${LIB_DIRECTORY}/protofiles/scripts/install_deps_debian.sh
-echo "STARTING THE SCRIPT"
