3d1163c19776aa23543c9df72d5fe9349472688b
Tyler Lum <tylergwlum@gmail.com>
2021-01-07 22:01:07 -0800

    Progress bar (#36)
    * Set north,south,east,west correctly to avoid segfault
    
    * Add progress bar
    
    * Remove unneeded part of progress bar
    
    * Add extra print details
    
    * Added counter to throttle progress bar
    
    * Fixed lint
    
    Co-authored-by: Kieran <kpfgallagher@gmail.com>

--- src/pathfinding/AStarPathfinder.cpp
+++ src/pathfinding/AStarPathfinder.cpp
@@ -1,6 +1,7 @@
 // Copyright 2017 UBC Sailbot
 
 #include "pathfinding/AStarPathfinder.h"
+#include "common/ProgressBar.h"
 
 #include <memory>
 #include <iostream>
@@ -28,16 +29,38 @@ Pathfinder::Result AStarPathfinder::Run() {
   open_set.emplace(start_, 0, heuristic_.calculate(start_, target_));
   visited[std::make_pair(start_, 0)] = VisitedStateData{0, std::make_pair(kInvalidHexVertexId, 0)};
 
+  const uint32_t max_h_cost = heuristic_.calculate(start_, target_);
+  uint32_t min_h_cost = max_h_cost;
+  ProgressBar progress_bar;
+  int progressCount = 0;
+
   // TODO(areksredzki): There is currently no check to see that the location is at all reachable.
   // Since there are no bounds on the time dimension, the pathfinder will run forever.
   while (!open_set.empty()) {
     AStarVertex current = open_set.top();
     open_set.pop();
 
-    // The best data for this IdTimeIndex up util now.
+    // The best data for this IdTimeIndex up until now.
     VisitedStateData current_data = visited[current.id_time_index()];
 
+    // Show on progress bar the closest we have gotten to goal
+    const uint32_t h_cost = heuristic_.calculate(current.hex_vertex_id(), target_);
+    min_h_cost = std::min(min_h_cost, h_cost);
+
+    if (progressCount > 10000) {
+      const double progress = 1.0 - static_cast<double>(min_h_cost) / max_h_cost;
+      progress_bar.update(progress);
+      const std::string text_after_progress_bar = "| Path cost = " + std::to_string(current.cost());
+      progress_bar.print(text_after_progress_bar);
+      progressCount = 0;
+    }
+
+    progressCount++;
+
     if (current.hex_vertex_id() == target_) {
+      // Flush progress bar
+      progress_bar.flush();
+
       stats_.closed_set_size = visited.size();
       stats_.open_set_size = open_set.size();
       return {ConstructPath(current.id_time_index(), visited), current_data.cost, current.time()};
