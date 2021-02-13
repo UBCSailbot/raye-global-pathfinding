0724ba14411409aaab0bcdf546d56557c8376617
Tyler Lum <tylergwlum@gmail.com>
2021-02-13 12:35:11 -0800

    Remove nanogui, as it is hindering integration with navigation (#42)
    * Remove nanogui, as it is hindering integration with navigation
    
    * Change https to ssh
    
    * Add azure pipelines setup for key
    
    * Try fix azure
    
    * Add comments, should be working by updating deploy key
    
    * Retry new key please work

--- lib/core_libs.cmake
+++ lib/core_libs.cmake
@@ -29,15 +29,3 @@ include_directories(${CURL_INCLUDE_DIRS})
 
 list(APPEND CORE_LIBS ${CURL_LIBRARIES})
 
-
-# Disable building extras we won't need (pure C++ project)
-set(NANOGUI_BUILD_EXAMPLE OFF CACHE BOOL " " FORCE)
-set(NANOGUI_BUILD_PYTHON  OFF CACHE BOOL " " FORCE)
-set(NANOGUI_INSTALL       OFF CACHE BOOL " " FORCE)
-
-# Add the configurations from nanogui
-add_subdirectory(lib/nanogui)
-include_directories(lib/nanogui/include)
-
-# For reliability of parallel build, make the NanoGUI targets dependencies
-set_property(TARGET glfw glfw_objects nanogui PROPERTY FOLDER "dependencies")
